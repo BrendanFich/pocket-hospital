fbc343af2b4778fd1b29a125a6b25736416d54f5
陈俊坚 <junjianch@gmail.com>
2019-10-10 17:39:19 +0800

    路由守卫

--- src/main.js
+++ src/main.js
@@ -19,6 +19,48 @@ Vue.use(VueAwesomeSwiper)
 Vue.use(MintUI)
 Vue.config.productionTip = false
 
+/*
+router.beforeEach((to, from, next) => {
+  // // 网页授权之获取code
+  // const code = getUrlParam("code"),
+  //   openId = window.localStorage.getItem("openId");
+  // if (!openId && !code) {
+  //   const appid = "wxd8de5e3e19b318ee",
+  //     url = encodeURIComponent(location.href.split('#')[0]);
+  //   window.location.href = 'http://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appid + '&redirect_uri=' + url + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+  // }
+  // //获取url中参数
+  // function getUrlParam(name) {
+  //   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  //   var r = window.location.search.substr(1).match(reg);
+  //   if (r != null) return decodeURI(r[2]);
+  //   return null;
+  function getUrlParam (name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
+    var r = window.location.search.substr(1).match(reg)
+    if (r != null) return decodeURI(r[2])
+    return null
+  }
+  let code = getUrlParam('code')
+
+  if (sessionStorage.getItem('store')) {
+    this.$store.replaceState(
+      Object.assign(
+        {},
+        this.$store.state,
+        JSON.parse(sessionStorage.getItem('store'))
+      )
+    )
+  }
+  console.log(code)
+  // 获取url中参数
+  const appid = 'wxd8de5e3e19b318ee'
+  const url = encodeURIComponent('http://qlyt.vicp.net:8197/api/user/vx_login')
+  if (!sessionStorage.getItem('code')) {
+    window.location.href = 'http://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appid + '&redirect_uri=' + url + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+  }
+})
+*/
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
