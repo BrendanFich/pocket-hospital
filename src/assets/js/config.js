e9fd118cada720e241e6d2b06fb28419b1a85a47
陈俊坚 <junjianch@gmail.com>
2020-08-16 18:32:52 +0800

    导航地址更换

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,7 +1,7 @@
 export const apiBaseUrl = process.env.NODE_ENV === 'production' ? 'http://' + window.location.host : 'http://wx.gdqlyt.com.cn'
 export const authUrl = process.env.NODE_ENV === 'production' ? 'http://' + window.location.host + '/api/user/wx_authorize' : 'http://wx.gdqlyt.com.cn/api/user/wx_authorize'
 
-export const latitude = 23.090656
-export const longitude = 113.1795
-export const hospitalName = '盐步医院(测试地址)'
-export const address = '盐步医院(测试地址)'
+export const latitude = 23.05101
+export const longitude = 112.463829
+export const hospitalName = '肇庆市端州区人民医院'
+export const address = '广东省肇庆市端州区城北路与宝月路交叉路口西南侧'
