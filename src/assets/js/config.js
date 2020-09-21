3fd8cdd8dfa85e3ea24bcfca358300510a01bce0
陈俊坚 <junjianch@gmail.com>
2020-09-21 11:59:06 +0800

    fix:接口多了一个/

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,5 +1,7 @@
-export const apiBaseUrl = process.env.NODE_ENV === 'production' ? window.location.href.split('#')[0] : 'http://dzyy.zhangfb.cn/weixin'
-export const authUrl = process.env.NODE_ENV === 'production' ? window.location.href.split('#')[0] + '/api/user/wx_authorize' : 'http://dzyy.zhangfb.cn/weixin/api/user/wx_authorize'
+let temp = window.location.href.split('#')[0]
+temp = temp.substring(0, temp.length - 1)
+export const apiBaseUrl = process.env.NODE_ENV === 'production' ? temp : 'http://dzyy.zhangfb.cn/weixin'
+export const authUrl = process.env.NODE_ENV === 'production' ? temp + '/api/user/wx_authorize' : 'http://dzyy.zhangfb.cn/weixin/api/user/wx_authorize'
 
 export const latitude = 23.05101
 export const longitude = 112.463829
