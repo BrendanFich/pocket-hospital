911c2d87eb9f6cd030b183105ceb2b48800cfb92
陈俊坚 <junjianch@gmail.com>
2021-01-06 16:44:04 +0800

    modify:测试获取图片base64

--- src/components/Mine/Mine/Mine.vue
+++ src/components/Mine/Mine/Mine.vue
@@ -81,8 +81,13 @@ export default {
           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: function (res) {
-            var localIds = res.localIds // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-            alert(localIds)
+            wx.getLocalImgData({
+              localId: res.localIds, // 图片的localID
+              success: function (res) {
+                var localData = res.localData // localData是图片的base64数据，可以用img标签显示
+                alert(localData)
+              }
+            })
           }
         })
       })
